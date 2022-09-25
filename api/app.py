acc8ab6a4ff3dd0418f1fea3d16daa96e95e4386
shamtang <119900035+shamtang@users.noreply.github.com>
2022-09-25 14:20:32 +0000

    add new method

--- api/app.py
+++ api/app.py
@@ -6,3 +6,10 @@ import numpy
 import librosa
 import openai
 import time
+import whisper
+
+app = Flask(__name__)
+set_api_key(os.getenv("ELEVEN_KEY"))
+app.secret_key = os.getenv("SECRET_KEY")
+openai.api_key = os.getenv("OPENAI_API_KEY")
+model = whisper.load_model("base")
