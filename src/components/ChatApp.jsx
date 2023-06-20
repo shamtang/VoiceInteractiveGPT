bfb8d00e7e6e96b9cd486ec4dadc77ca8c7c2318
shamtang <119900035+shamtang@users.noreply.github.com>
2023-06-20 11:14:01 +0000

    Update info.js

--- src/components/ChatApp.jsx
+++ src/components/ChatApp.jsx
@@ -8,4 +8,7 @@ function ChatApp() {
   const [loading, setLoading] = useState(false);
   const [audioBlob, setAudioBlob] = useState(null);
   const [audioSrc, setAudioSrc] = useState('');
- 
+  const [messages, setMessages] = useState([
+    { id: 1, user: false, content: "Hello, I'm ChatGPT" },
+    { id: 2, user: true, content: "Hi, I'm the User" },
+    { id: 3, user:
