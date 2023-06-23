1667166f2d2f5d9794437db5f51f408f51de0aa7
shamtang <119900035+shamtang@users.noreply.github.com>
2023-06-23 18:41:05 +0000

    Renamed app

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,2 +1,12 @@
 import React from 'react';
-import 'boo
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function Loading() {
+  return (
+    <div className='loading mt-5'>
+      <div className='spinner-border text-primary' role='status'></div>
+    </div>
+  );
+}
+
+export default
