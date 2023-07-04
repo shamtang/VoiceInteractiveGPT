adda6ebfbb895ecc68c44b8276aa84ce2dcf9d1a
shamtang <119900035+shamtang@users.noreply.github.com>
2023-07-04 09:51:32 +0000

    massive cleanup

--- src/index.js
+++ src/index.js
@@ -2,4 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import * as serviceWorker from "./ser
+import * as serviceWorker from "./serviceWorker";
+
+// Use createRoot instead of render for React 17
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+// If you want your app to work offline and load faster, 
