7e6fd5159a907944c0265b8e01df142bbeabf3e4
shamtang <119900035+shamtang@users.noreply.github.com>
2023-07-04 15:51:36 +0000

    add more to readme

--- src/index.js
+++ src/index.js
@@ -7,4 +7,7 @@ import * as serviceWorker from "./serviceWorker";
 // Use createRoot instead of render for React 17
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
 
-// If you want your app to work offline and load faster, 
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
