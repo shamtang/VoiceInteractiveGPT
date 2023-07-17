ce3442768e41824a5ff0ffef68de1af8c9282c6c
shamtang <119900035+shamtang@users.noreply.github.com>
2023-07-17 21:36:55 +0000

    merging develop

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -11,4 +11,8 @@
 // opt-in, read https://bit.ly/CRA-PWA
 
 const isLocalhost = Boolean(
-  window.location.hostname === 'loc
+  window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.0/8 are considered localhost for IPv4.
+    window.lo
