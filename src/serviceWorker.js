123b5c6ba0265a718df71214c01f6493be10371b
shamtang <119900035+shamtang@users.noreply.github.com>
2023-07-19 16:55:46 +0000

    Removed generated files.

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -15,4 +15,10 @@ const isLocalhost = Boolean(
     // [::1] is the IPv6 localhost address.
     window.location.hostname === '[::1]' ||
     // 127.0.0.0/8 are considered localhost for IPv4.
-    window.lo
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+export function register(config) {
+  if (process.env.NODE_ENV === 'production' && 'se
