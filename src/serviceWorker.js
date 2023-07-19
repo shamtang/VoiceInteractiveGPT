88f48dc29ecb9d90bf761d39a32bfe3cce987e94
shamtang <119900035+shamtang@users.noreply.github.com>
2023-07-19 20:05:22 +0000

    Update arch-installer-romanian.conf

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -21,4 +21,7 @@ const isLocalhost = Boolean(
 );
 
 export function register(config) {
-  if (process.env.NODE_ENV === 'production' && 'se
+  if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+    // The URL constructor is available in all browsers that support SW.
+    const publicUrl = new URL(process.env.PUBLIC_URL, window.location.href);
+    if (publicUr
