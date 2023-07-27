8e87932336db471cc4741a178c3e052cb16c3f73
shamtang <119900035+shamtang@users.noreply.github.com>
2023-07-27 09:58:41 +0000

    Update Test.html

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -36,4 +36,8 @@ export function register(config) {
 
       if (isLocalhost) {
         // This is running on localhost. Let's check if a service worker still exists or not.
-        checkValidServ
+        checkValidServiceWorker(swUrl, config);
+
+        // Add some additional logging to localhost, pointing developers to the
+        // service worker/PWA documentation.
+        navigator.serviceWorker
