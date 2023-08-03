83d54e6a9598fad9b300ed85854f41e2ecd5fce5
shamtang <119900035+shamtang@users.noreply.github.com>
2023-08-03 20:41:01 +0000

    add clarifying comment

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -60,3 +60,8 @@ function registerValidSW(swUrl, config) {
     .then(registration => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
+        if (installingWorker == null) {
+          return;
+        }
+        installingWorker.onstatechange = () => {
+          if (installingWorker.state === 'installed
