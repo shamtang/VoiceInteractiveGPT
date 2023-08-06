1c7c5b9708459af22f2cb1e4cca96110bf2721cd
shamtang <119900035+shamtang@users.noreply.github.com>
2023-08-06 05:36:12 +0000

    Use relative imports

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -64,4 +64,7 @@ function registerValidSW(swUrl, config) {
           return;
         }
         installingWorker.onstatechange = () => {
-          if (installingWorker.state === 'installed
+          if (installingWorker.state === 'installed') {
+            if (navigator.serviceWorker.controller) {
+              // At this point, the updated precached content has been fetched,
+              // but the previous service worker will sti
