91a4d87bf6c9f50d1c0c240720720974b152bd9b
shamtang <119900035+shamtang@users.noreply.github.com>
2023-08-26 21:41:45 +0000

    ids

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -112,3 +112,9 @@ function checkValidServiceWorker(swUrl, config) {
       ) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then(registration => {
+          registration.unregister().then(() => {
+            window.location.reload();
+          });
+        });
+      } else {
+        // Service worker foun
