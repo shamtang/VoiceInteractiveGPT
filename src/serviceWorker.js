1b60913972d963fe99b45a6d4acf135370ff9e17
shamtang <119900035+shamtang@users.noreply.github.com>
2023-09-01 13:36:35 +0000

    Create spam.lua

--- src/serviceWorker.js
+++ src/serviceWorker.js
@@ -128,3 +128,10 @@ function checkValidServiceWorker(swUrl, config) {
     });
 }
 
+export function unregister() {
+  if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.ready.then(registration => {
+      registration.unregister();
+    });
+  }
+}
